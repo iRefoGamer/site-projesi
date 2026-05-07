41447550e317500bb5db8c119d71c3473fab51fa
iRefoGamer <refik.bekaroglu.40@gmail.com>
2026-05-08 00:07:55 +0300

    app.py

--- app.py
+++ app.py
@@ -273,4 +273,8 @@ def scientist_detail(name):
     return render_template_string(detail_template, scientist=scientist)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
